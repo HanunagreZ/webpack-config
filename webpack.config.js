1436e3a3e60d9fd63833b10e9c9ca16aa9c28595
HanunagreZ <hanunagrez@yandex.ru>
2024-03-10 16:53:50 +0300

    feat: add prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env) => ({
-  mode: env.mode ?? "development",
-  entry: path.resolve(__dirname, "src", "index.ts"),
+  mode: env.mode ?? 'development',
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-    assetModuleFilename: "assets/[name].[ext]",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[name].[ext]',
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
+      template: path.resolve(__dirname, 'src', 'index.html'),
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
+      filename: '[name].[contenthash].css',
     }),
     new CopyPlugin({
-      patterns: [{ from: "src/assets", to: "media/" }],
+      patterns: [{ from: 'src/assets', to: 'media/' }],
     }),
     new CleanWebpackPlugin(),
   ],
@@ -29,30 +29,30 @@ module.exports = (env) => ({
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
 
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   devServer: {
     port: 5500,
