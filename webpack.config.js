afafb0bf85feb94b987d11ee2b960dbcc34fd6d5
HanunagreZ <hanunagrez@yandex.ru>
2024-03-10 13:05:54 +0300

    feat: add ts

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    port: 5500,
+    open: true,
+  },
+};
