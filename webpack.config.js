5f9f89d0a5541c8152fa87b8b0e2e2c0552a940d
HanunagreZ <hanunagrez@yandex.ru>
2024-03-10 16:41:15 +0300

    feat: add more addons

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
+module.exports = (env) => ({
+  mode: env.mode ?? "development",
   entry: path.resolve(__dirname, "src", "index.ts"),
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+    assetModuleFilename: "assets/[name].[ext]",
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "media/" }],
+    }),
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
@@ -20,6 +31,22 @@ module.exports = {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
+        type: "asset/inline",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -30,5 +57,6 @@ module.exports = {
   devServer: {
     port: 5500,
     open: true,
+    hot: true,
   },
-};
+});
